3cc950025b9e5e6261d9a336a0f973e59012ad6c
Markus Olsson <j.markus.olsson@gmail.com>
2014-02-18 12:50:57 +0100

    Clean up the connectables

--- RxSpy.Server/Observables/ConnectableOperatorConnection.cs
+++ RxSpy.Server/Observables/ConnectableOperatorConnection.cs
@@ -1,22 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Disposables;
 using System.Reactive.Subjects;
 using RxSpy.Events;
 
 namespace RxSpy.Observables
 {
-    internal class ConnectableOperatorConnection<T> : OperatorConnection<T>, IOperatorObservable, IConnectableObservable<T>
+    internal class ConnectableOperatorConnection<T> : OperatorConnection<T>, IConnectableObservable<T>
     {
         readonly IConnectableObservable<T> _connectableObservable;
 
         public ConnectableOperatorConnection(RxSpySession session, IConnectableObservable<T> parent, OperatorInfo childInfo)
             : base(session, parent, childInfo)
         {
+            _connectableObservable = parent;
         }
 
         public IDisposable Connect()
         {
-            return _connectableObservable.Connect();
+            var connectionId = Session.EnqueueEvent(Event.Connect(OperatorInfo));
+            var disp = _connectableObservable.Connect();
+
+            return Disposable.Create(() =>
+            {
+                disp.Dispose();
+                Session.EnqueueEvent(Event.Disconnect(connectionId));
+            });
         }
     }
 }
--- RxSpy.Server/Observables/ConnectableOperatorObservable.cs
+++ RxSpy.Server/Observables/ConnectableOperatorObservable.cs
@@ -6,14 +6,14 @@ using RxSpy.Events;
 
 namespace RxSpy.Observables
 {
-    internal class ConnectableOperatorObservable<T>: OperatorObservable<T>, IConnectableObservable<T>
+    internal class ConnectableOperatorObservable<T> : OperatorObservable<T>, IConnectableObservable<T>
     {
         readonly IConnectableObservable<T> _connectableObservable;
 
-        public ConnectableOperatorObservable(RxSpySession session, IConnectableObservable<T> source, OperatorInfo operatorInfo)
-            : base(session, source, operatorInfo)
+        public ConnectableOperatorObservable(RxSpySession session, IConnectableObservable<T> parent, OperatorInfo operatorInfo)
+            : base(session, parent, operatorInfo)
         {
-            _connectableObservable = source;
+            _connectableObservable = parent;
         }
 
         public IDisposable Connect()
@@ -21,7 +21,8 @@ namespace RxSpy.Observables
             var connectionId = Session.EnqueueEvent(Event.Connect(OperatorInfo));
             var disp = _connectableObservable.Connect();
 
-            return Disposable.Create(() => {
+            return Disposable.Create(() =>
+            {
                 disp.Dispose();
                 Session.EnqueueEvent(Event.Disconnect(connectionId));
             });
