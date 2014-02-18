83b3d939fb709012aea3fb542cebe07e61f9e6b7
Markus Olsson <j.markus.olsson@gmail.com>
2014-02-18 12:52:08 +0100

    Flip the connection/observable relationship
    Connections can't be responsible for the event handling of an
    observable.

--- RxSpy.Server/Observables/OperatorConnection.cs
+++ RxSpy.Server/Observables/OperatorConnection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reactive.Disposables;
 using RxSpy.Events;
 
 namespace RxSpy.Observables
@@ -10,42 +11,8 @@ namespace RxSpy.Observables
         private OperatorInfo _parentInfo;
         private RxSpySession _session;
 
-        public OperatorInfo OperatorInfo
-        {
-            get { return _childInfo; }
-        }
-
-        class Observer : IObserver<T>, IOperatorObservable
-        {
-            readonly OperatorConnection<T> _connection;
-            readonly IObserver<T> _inner;
-
-            public OperatorInfo OperatorInfo { get { return _connection._childInfo; } }
-
-            public Observer(OperatorConnection<T> connection, IObserver<T> inner)
-            {
-                _connection = connection;
-                _inner = inner;
-            }
-
-            public void OnCompleted()
-            {
-                _connection._session.EnqueueEvent(Event.OnCompleted(_connection._parentInfo));
-                _inner.OnCompleted();
-            }
-
-            public void OnError(Exception error)
-            {
-                _connection._session.EnqueueEvent(Event.OnError(_connection._parentInfo, error));
-                _inner.OnError(error);
-            }
-
-            public void OnNext(T value)
-            {
-                _connection._session.EnqueueEvent(Event.OnNext(_connection._parentInfo, typeof(T), value));
-                _inner.OnNext(value);
-            }
-        }
+        public OperatorInfo OperatorInfo { get { return _childInfo; } }
+        protected RxSpySession Session { get { return _session; } }
 
         public OperatorConnection(RxSpySession session, IObservable<T> parent, OperatorInfo childInfo)
         {
@@ -62,8 +29,15 @@ namespace RxSpy.Observables
 
         public virtual IDisposable Subscribe(IObserver<T> observer)
         {
-            _session.EnqueueEvent(Event.Subscribe(_childInfo, _parentInfo));
-            return _parent.Subscribe(new Observer(this, observer));
+            var subscriptionId = _session.EnqueueEvent(Event.Subscribe(_childInfo, _parentInfo));
+
+            var disp = _parent.Subscribe(observer);
+
+            return Disposable.Create(() =>
+            {
+                disp.Dispose();
+                _session.EnqueueEvent(Event.Unsubscribe(subscriptionId));
+            });
         }
     }
 }
--- RxSpy.Server/Observables/OperatorObservable.cs
+++ RxSpy.Server/Observables/OperatorObservable.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reactive.Disposables;
 using System.Reactive.Subjects;
+using System.Threading;
 using RxSpy.Events;
 using RxSpy.Utils;
 
@@ -10,10 +11,72 @@ namespace RxSpy.Observables
     {
         readonly OperatorInfo _operatorInfo;
         readonly RxSpySession _session;
-        private IObservable<T> _inner;
+        readonly IObservable<T> _inner;
 
         protected RxSpySession Session { get { return _session; } }
         public OperatorInfo OperatorInfo { get { return _operatorInfo; } }
+
+        // We can't subscribe an extra observer to out inner observable just for the event stuff
+        // since that could potentially modify the behavior of the inner (like if it was designed 
+        // for one subscription only. So instead of doing that we wrap all incoming observers in
+        // a private class which forwards all signals while keeping track of whether or not it's
+        // the wrapper currently responsible for reporting events.
+        Observer _currentlyReportingObserver;
+
+        class Observer: IObserver<T>, IDisposable
+        {
+            readonly OperatorObservable<T> _parent;
+            readonly IObserver<T> _inner;
+
+            bool _isReporting = false;
+
+            public Observer(OperatorObservable<T> parent, IObserver<T> inner)
+            {
+                _parent = parent;
+                _inner = inner;
+            }
+
+            bool isReporting()
+            {
+                if (!_isReporting && Interlocked.CompareExchange(ref _parent._currentlyReportingObserver, this, null) == null)
+                    _isReporting = true;
+
+                return _isReporting;
+            }
+
+            public void OnCompleted()
+            {
+                if (isReporting())
+                    _parent._session.EnqueueEvent(Event.OnCompleted(_parent._operatorInfo));
+
+                _inner.OnCompleted();
+            }
+
+            public void OnError(Exception error)
+            {
+                if (isReporting())
+                    _parent._session.EnqueueEvent(Event.OnError(_parent._operatorInfo, error));
+                
+                _inner.OnError(error);
+            }
+
+            public void OnNext(T value)
+            {
+                if (isReporting())
+                    _parent._session.EnqueueEvent(Event.OnNext(_parent._operatorInfo, typeof(T), value));
+
+                _inner.OnNext(value);
+            }
+
+            public void Dispose() 
+            {
+                if (_isReporting)
+                {
+                    Interlocked.CompareExchange(ref _parent._currentlyReportingObserver, null, this);
+                    _isReporting = false;
+                }
+            }
+        }
 
         public OperatorObservable(RxSpySession session, IObservable<T> inner, OperatorInfo operatorInfo)
         {
@@ -29,13 +92,10 @@ namespace RxSpy.Observables
 
         public IDisposable Subscribe(IObserver<T> observer)
         {
-            var oobs = observer as IOperatorObservable;
+            var obs = new Observer(this, observer);
+            var disp = _inner.Subscribe(obs);
 
-            if (oobs != null)
-                return _inner.Subscribe(observer);
-
-            _session.EnqueueEvent(Event.Subscribe(_session.GetOperatorInfoFor(observer), _operatorInfo));
-            return _inner.Subscribe(new OperatorObserver<T>(_session, observer, _operatorInfo));
+            return new CompositeDisposable(disp, obs);
         }
 
         public override string ToString()
