7c5cfd2bf82f110ac6fce508558fbd76375064c9
Markus Olsson <j.markus.olsson@gmail.com>
2014-02-22 20:58:45 +0100

    Cache debugger display delegates
    Speed boost!

---
+++ RxSpy.Server/Utils/DebuggerDisplayFormatter.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace RxSpy.Utils
+{
+    public class DebuggerDisplayFormatter
+    {
+        readonly static ConcurrentDictionary<Type, Lazy<Func<object, string>>> _cachedFormatters =
+            new ConcurrentDictionary<Type, Lazy<Func<object, string>>>();
+
+        static readonly Regex DebuggerDisplayPropertyRe = new Regex(@"\{\s*(\w[\w\d]+)(,nq)?\s*\}");
+
+        public static bool TryFormat(Type type, object target, out string value)
+        {
+            var formatter = _cachedFormatters.GetOrAdd(type, CreateFormatter);
+
+            if (formatter == null)
+            {
+                value = null;
+                return false;
+            }
+
+            value = formatter.Value(target);
+            return true;
+        }
+
+        static Lazy<Func<object, string>> CreateFormatter(Type type)
+        {
+            var debuggerDisplayAttributes = type.GetCustomAttributes(typeof(DebuggerDisplayAttribute), false);
+
+            if (debuggerDisplayAttributes == null || debuggerDisplayAttributes.Length == 0)
+            {
+                return null;
+            }
+
+            var attribute = (DebuggerDisplayAttribute)debuggerDisplayAttributes[0];
+
+            return new Lazy<Func<object, string>>(
+                () => BuildFormatterDelegate(type, attribute.Value),
+                LazyThreadSafetyMode.ExecutionAndPublication
+            );
+        }
+
+        static Func<object, string> BuildFormatterDelegate(Type type, string format)
+        {
+            // We only support simple property getters for now, no method invocation
+
+            try
+            {
+                int count = 0;
+
+                var substitionFuncs = new List<Func<object, object>>();
+
+                string newFormat = DebuggerDisplayPropertyRe.Replace(format, m =>
+                {
+                    int p = count++;
+
+                    var propertyName = m.Groups[1].Value;
+                    //var propertyValue = GetValueForFieldOrProperty(propertyName, value, type);
+
+                    substitionFuncs.Add(CreatePropertyValueDelegate(type, propertyName));
+                    if (!m.Groups[2].Success)
+                    {
+                        return "\"{" + p + "}\"";
+                    }
+                    else
+                    {
+                        return "{" + p + "}";
+                    }
+                });
+
+                return o =>
+                {
+                    return string.Format(newFormat, substitionFuncs.Select(x => x(o)).ToArray());
+                };
+            }
+            catch (Exception exc)
+            {
+                return new Func<object, string>(_ => "Could not create debugger display formatter " + exc.Message);
+            }
+        }
+
+        static Func<object, object> CreatePropertyValueDelegate(Type type, string propertyName)
+        {
+            var propertyInfo = type.GetProperty(propertyName);
+
+            if (propertyInfo != null)
+            {
+                return o => propertyInfo.GetValue(o) ?? "null";
+            }
+
+            var fieldInfo = type.GetField(propertyName);
+
+            if (fieldInfo != null)
+            {
+                return o => fieldInfo.GetValue(o) ?? "null";
+            }
+
+            return o => "No such property or field " + propertyName; 
+        }
+    }
+}
--- RxSpy.Server/Utils/ValueFormatter.cs
+++ RxSpy.Server/Utils/ValueFormatter.cs
@@ -16,12 +16,18 @@ namespace RxSpy.Utils
             if (value == null)
                 return "<null>";
 
+            return ToString(value, value.GetType());
+        }
+
+        public static string ToString(object value, Type valueType)
+        {
+            if (value == null)
+                return "<null>";
+
             var s = value as string;
 
             if (s != null)
                 return '"' + s + '"';
-
-            var valueType = value.GetType();
 
             if (valueType.IsArray)
             {
@@ -48,68 +54,12 @@ namespace RxSpy.Utils
                 return TypeUtils.ToFriendlyName(valueType) + " {" + string.Join(", ", list.Cast<object>().Select(ToString)) + "}";
             }
 
-            var debuggerDisplayAttributes = valueType.GetCustomAttributes(typeof(DebuggerDisplayAttribute), false);
+            string debuggerDisplayValue;
 
-            if (debuggerDisplayAttributes != null && debuggerDisplayAttributes.Length > 0)
-            {
-                return FormatWithDebuggerDisplay(value, (DebuggerDisplayAttribute)debuggerDisplayAttributes[0]);
-            }
+            if (DebuggerDisplayFormatter.TryFormat(valueType, value, out debuggerDisplayValue))
+                return debuggerDisplayValue;
 
             return Convert.ToString(value);
-        }
-
-        static string FormatWithDebuggerDisplay(object value, DebuggerDisplayAttribute attribute)
-        {
-            return FormatWithDebuggerDisplay(value, attribute.Value);
-        }
-
-        static readonly Regex DebuggerDisplayPropertyRe = new Regex(@"\{\s*(\w[\w\d]+)(,nq)?\s*\}");
-
-        public static string FormatWithDebuggerDisplay(object value, string debuggerDisplayFormat)
-        {
-            if (value == null)
-            {
-                throw new ArgumentNullException("value");
-            }
-
-            // We only support simple property getters for now, no method invocation
-
-            try
-            {
-                var type = value.GetType();
-
-                return DebuggerDisplayPropertyRe.Replace(debuggerDisplayFormat, m =>
-                {
-                    var propertyName = m.Groups[1].Value;
-                    var propertyValue = GetValueForFieldOrProperty(propertyName, value, type);
-
-                    if (!m.Groups[2].Success)
-                    {
-                        return '"' + propertyValue + '"';
-                    }
-
-                    return propertyValue;
-                });
-            }
-            catch (Exception exc)
-            {
-                return "Invocation failed " + exc.Message;
-            }
-        }
-
-        static string GetValueForFieldOrProperty(string propertyName, object value, Type type)
-        {
-            var property = type.GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.GetProperty);
-
-            if (property != null)
-                return Convert.ToString(property.GetValue(value));
-
-            var field = type.GetField(propertyName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.GetField);
-
-            if (field != null)
-                return Convert.ToString(field.GetValue(value));
-
-            return "<err>";
         }
     }
 }
