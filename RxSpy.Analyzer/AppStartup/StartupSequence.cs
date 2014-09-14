c47456684a9e82131e38d2421b5980b58cc4b362
Markus <j.markus.olsson@gmail.com>
2014-09-14 23:01:11 +0200

    Groundwork for analyzer ui

---
+++ RxSpy.Analyzer/AppStartup/StartupSequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using RxSpy.Analyzer.ViewModels;
+using RxSpy.Analyzer.Views;
+using Splat;
+
+namespace RxSpy.Analyzer.AppStartup
+{
+    public static class StartupSequence
+    {
+        public static void Start()
+        {
+            RegisterViewsAndViewModels();
+        }
+
+        private static void RegisterViewsAndViewModels()
+        {
+            var mainViewModel = new MainViewModel();
+
+            Locator.CurrentMutable.RegisterConstant(mainViewModel, typeof(MainViewModel));
+
+            Locator.CurrentMutable.Register(() => new MainView(), typeof(IViewFor<MainViewModel>));
+        }
+    }
+}
